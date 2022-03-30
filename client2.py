396b34ab22f84c8f26b4df219b745c6bc33bff3e
OUM <aoum@et.esiea.fr>
2022-03-30 09:43:08 +0200

    commit

--- client2.py
+++ client2.py
@@ -21,11 +21,15 @@ class MainWindow(QWidget):
         self.initUI()
 
     def initUI(self):
+        #Creation Interface de la Window#
+        #Donne la taille de la Window"
         self.setWindowTitle("Locate your IP : (OUM)")
         self.setFixedSize(400, 400)
+        #Creation d'un label et d'une zone d'input"
         self.label1 = QLabel("Enter your IP:", self)
         self.text = QLineEdit(self)
         self.text.setText(self.get_ip())
+        #On bouge le label et la zone d'input a l'endroit voulue#
         self.label1.move(10,25)
         self.text.move(10, 50)
         
@@ -44,12 +48,13 @@ class MainWindow(QWidget):
         self.button = QPushButton("Send", self)
         self.button.move(10, 250)
 
+        #quand on appuie sur le bouton on lance la fonction on_click#
         self.button.pressed.connect(self.on_click)
-
         self.show()
 
 
     def get_ip(self):
+        #Recupere Ip automatiquement#
         url="https://api64.ipify.org?format=json"
         res = self.__query(url)
         if res:
@@ -58,16 +63,21 @@ class MainWindow(QWidget):
             return ""
 
     def on_click(self):
+        #on recupere les value des inputs#
         hostname = self.text.text()
         key = self.text3.text()
         hostip = self.text4.text()
+        #on les rentre dans le lien http#
         res = self.__query("http://"+hostip+"/ip/"+hostname+"?key="+key)
         if res and type(res)==dict:
+            #on recupere la latitude et la longitude#
             lat=str(res["latitude"])
             long=str(res["longitude"])
+            #on ecrit la latitude et la longitude dans la window#
             self.label2.setText("latitude="+lat+"\nlongitude="+long)
             self.label2.adjustSize()
             self.show()
+            #on ouvre une page web a la latitude et longitude recuperé#
             webbrowser.open(url="https://www.openstreetmap.org/?mlat="+lat+"&mlon="+long+"#map=12",new=0)
 
     def __query(self, url):
